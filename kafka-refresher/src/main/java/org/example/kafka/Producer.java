0982594e080c170bb78d51fdebfda0cd9cd5b5dd
LakshmiPrasanna <prassannamaddipati@gmail.com>
2024-09-02 20:52:13 -0400

    working version kafka producer consumer in java

---
+++ kafka-refresher/src/main/java/org/example/kafka/Producer.java
@@ -0,0 +1,49 @@
+package org.example.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class Producer {
+    private static final Logger log = LoggerFactory.getLogger(Producer.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Producer!");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+
+        // connect to Localhost
+//        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // connect to Conduktor Playground
+        properties.setProperty("bootstrap.servers", "eternal-mastodon-7166-us1-kafka.upstash.io:9092");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
+        properties.setProperty("sasl.mechanism", "PLAIN");
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a Producer Record
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demo_java", "hello world");
+
+        // send data
+        producer.send(producerRecord);
+
+        // tell the producer to send all data and block until done -- synchronous
+        producer.flush();
+
+        // flush and close the producer
+        producer.close();
+    }
+}
